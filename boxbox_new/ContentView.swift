41fc67a67f5db588a81b19fcf83c32afad7bdc93
Nidtaya94 <57827838+Nidtaya94@users.noreply.github.com>
2020-01-05 09:42:12 +0700

    Initial Commit

---
+++ boxbox_new/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  boxbox_new
+//
+//  Created by NIdtaya Jamchoi on 5/1/2563 BE.
+//  Copyright © 2563 Silpakorn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
